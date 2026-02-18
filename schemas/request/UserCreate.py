e1f796b8a21bf7f34751dc9e2b6151b9ca4c5e59
Juan David <jdroserot@udistrital.edu.co>
2026-02-17 19:11:09 -0500

    Move request Dtos and create user Response

--- schemas/request/UserCreate.py
+++ schemas/request/UserCreate.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel, Field
 
 
 class UserCreate(BaseModel):
+    id: int = Field(...)
     username: str = Field(...,max_length=50)
     password: str = Field(...,max_length=50)
     id_rol: int = Field(...,ge=0)
