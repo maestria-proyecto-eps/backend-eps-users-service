dfb31fa0220fe92e7f1faf2aff3e55941a041da6
Jordi <joricor@udistrital.edu.co>
2026-02-18 19:35:07 -0500

    Fix services+routers

--- services/UserService.py
+++ services/UserService.py
@@ -1,18 +1,20 @@
 
 from models import Usuario
 from schemas.request import UserCreate
+from schemas.response.UserResponse import UserResponse
 from services.repositories import RolRepository
-from services.repositories.UserRepository import userRepository
+from services.repositories.UserRepository import UserRepository
 
 
-class userService:
-    def __init__(self, repo: userRepository, rolRepo: RolRepository):
+class UserService:
+    def __init__(self, repo: UserRepository, rolRepo: RolRepository):
         self.repo = repo
         self.rolRepo = rolRepo
-    def AddUser(self, userData: UserCreate) -> int:
-        if(self.repo.exists_by_id(userData.id)):
-            raise ValueError("ID ya registrado")
-        if(self.repo.exists_by_id(userData.username)):
+    def AddUser(self, userData: UserCreate) -> UserResponse:
+        # El Usuario no tiene Identificacion
+        #if(self.repo.exists_by_id(userData.id)):
+         #   raise ValueError("ID ya registrado")
+        if(self.repo.exists_by_username(userData.username)):
             raise ValueError("Username ya registrado")
         if(not self.rolRepo.exists_by_id(userData.id_rol)):
             raise ValueError("El rol no existe")
@@ -23,4 +25,4 @@ class userService:
         self.repo.db.commit()
         self.repo.db.refresh(user)
 
-        return user.id
+        return UserResponse.model_validate(user)
--- services/repositories/UserRepository.py
+++ services/repositories/UserRepository.py
@@ -2,14 +2,14 @@ from sqlalchemy import select
 from sqlalchemy.orm import Session
 from models.Usuario import Usuario
 
-class userRepository:
+class UserRepository:
     def __init__(self, db: Session):
         self.db = db
     def exists_by_id(self, id: int) -> bool:
-        stmt = select(Usuario.id).where(Usuario.id_usuario == id)
+        stmt = select(Usuario.id_usuario).where(Usuario.id_usuario == id)
         return self.db.scalar(stmt) is not None
     def exists_by_username(self, username: str) -> bool:
-        stmt = select(Usuario.id).where(Usuario.username == username)
+        stmt = select(Usuario.id_usuario).where(Usuario.username == username)
         return self.db.scalar(stmt) is not None
     def get_by_id(self, user_id: int) -> Usuario | None:
         stmt = select(Usuario).where(Usuario.id == user_id)
