207ce04e047cbaf3f1b49d1aaffded7f9c6b928f
Jordi <joricor@udistrital.edu.co>
2026-02-18 19:48:36 -0500

    add usuarios relationship

--- models/Rol.py
+++ models/Rol.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import  relationship
 from db.session import Base 
 
 
@@ -6,6 +7,8 @@ class Role(Base):
     __tablename__ = "roles"
     id_rol= Column(Integer, primary_key=True)
     nombre_rol= Column(String(50), nullable=False)
+
+    usuarios = relationship("Usuario", back_populates="rol")
 
     def __repr__(self):
         return f"<Rol(id_rol='{self.id_rol}', nombre_rol='{self.nombre_rol}')>"
