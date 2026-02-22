3f5b1e3cff2e2e9aaae1156382a480c16b8e4616
Juan David <jdroserot@udistrital.edu.co>
2026-02-21 19:38:21 -0500

    Update userService

--- services/UserService.py
+++ services/UserService.py
@@ -1,6 +1,7 @@
 
-from models import Usuario
+from models.Usuario import Usuario
 from schemas.request import UserCreate
+from schemas.response.GenericResponse import Response
 from schemas.response.UserResponse import UserResponse
 from services.repositories import RolRepository
 from services.repositories.UserRepository import UserRepository
@@ -10,19 +11,17 @@ class UserService:
     def __init__(self, repo: UserRepository, rolRepo: RolRepository):
         self.repo = repo
         self.rolRepo = rolRepo
-    def AddUser(self, userData: UserCreate) -> UserResponse:
-        # El Usuario no tiene Identificacion
-        #if(self.repo.exists_by_id(userData.id)):
-         #   raise ValueError("ID ya registrado")
+    def AddUser(self, userData: UserCreate):
         if(self.repo.exists_by_username(userData.username)):
-            raise ValueError("Username ya registrado")
+            return Response.error("Username ya registrado")
         if(not self.rolRepo.exists_by_id(userData.id_rol)):
-            raise ValueError("El rol no existe")
-        
+            return Response.error("Rol no registrado")
         user = Usuario(**userData.dict())
 
-        user = self.repo.create(user)
+        user.estado=1
+
+        self.repo.add_user(user)
         self.repo.db.commit()
         self.repo.db.refresh(user)
 
-        return UserResponse.model_validate(user)
+        return Response.ok(UserResponse.model_validate(user),"Usuario creado exitosamente")
