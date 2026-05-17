2a8f2a09e95ef463af86f70ae5fc683696f4c2f3
gyuheeJo <davidgyuhee@gmail.com>
2026-05-17 16:06:31 -0500

    tests arreglado

--- core/config.py
+++ core/config.py
@@ -1,25 +1,30 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
-    # --- CONFIGURACIÓN BASE DE DATOS ADMINISTRATIVA ---
-    DB_ADMIN_USER: str
-    DB_ADMIN_PASSWORD: str
-    DB_ADMIN_HOST: str
+    # ADMIN
+    DB_ADMIN_USER: str = "test"
+    DB_ADMIN_PASSWORD: str = "test"
+    DB_ADMIN_HOST: str = "localhost"
     DB_ADMIN_PORT: int = 5432
-    DB_ADMIN_NAME: str
+    DB_ADMIN_NAME: str = "test_db"
 
-    # --- CONFIGURACIÓN BASE DE DATOS OPERATIVA ---
-    DB_OP_USER: str
-    DB_OP_PASSWORD: str
-    DB_OP_HOST: str
+    # OPERATIVA
+    DB_OP_USER: str = "test"
+    DB_OP_PASSWORD: str = "test"
+    DB_OP_HOST: str = "localhost"
     DB_OP_PORT: int = 5432
-    DB_OP_NAME: str
+    DB_OP_NAME: str = "test_db"
 
-    # --- JWT ---
-    JWT_EXPIRES_MINUTES: int
-    JWT_SECRET: str
-    JWT_ALGORITHM: str
+    # JWT
+    JWT_EXPIRES_MINUTES: int = 60
+    JWT_SECRET: str = "test-secret"
+    JWT_ALGORITHM: str = "HS256"
 
-    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore"
+    )
+
 
 settings = Settings()
